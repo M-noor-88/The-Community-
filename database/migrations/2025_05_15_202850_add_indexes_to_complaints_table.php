16017834c9910d1ca3c6b16348b73296e6cab30b
M-noor-88 <taknoor45@gmail.com>
2025-08-20 10:16:52 +0300

    remove index

--- database/migrations/2025_05_15_202850_add_indexes_to_complaints_table.php
+++ database/migrations/2025_05_15_202850_add_indexes_to_complaints_table.php
@@ -10,7 +10,6 @@ class AddIndexesToComplaintsTable extends Migration
     {
         Schema::table('complaints', function (Blueprint $table) {
             $table->index('status');               // Index on status column
-            $table->index('complaint_category_id'); // Index on category_id
             $table->index('location_id');          // Index on location_id
         });
     }
@@ -18,9 +17,8 @@ class AddIndexesToComplaintsTable extends Migration
     public function down()
     {
         Schema::table('complaints', function (Blueprint $table) {
-            $table->dropIndex(['status']);
-            $table->dropIndex(['complaint_category_id']);
-            $table->dropIndex(['location_id']);
+            $table->dropIndex('complaints_status_index');
+            $table->dropIndex('complaints_location_id_index');
             // $table->dropIndex(['user_id']);
         });
     }
